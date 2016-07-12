1b7c345b39fc563c986e5a7288409e56790c2f5f
thilko <thilko.richter@googlemail.com>
2016-07-12 09:21:01 +0200

    better logging

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 require('events').EventEmitter.prototype._maxListeners = Infinity;
 const https = require("https");
 const http = require("http");
+const apiKey = process.env.api_key;
 
 var data = "";
 var json;
 var deleteCounter = 0;
 var totalCount = 0;
-var c = https.get('https://api.pinboard.in/v1/posts/all?format=json&auth_token=thilko:CC3DBDA67FC2060DE710', (res) => {
+
+https.get('https://api.pinboard.in/v1/posts/all?format=json&auth_token=' + apiKey, (res) => {
       res.on('data', (d) => { data += d; });
       res.on('error', (e) => { console.error(e); });
       res.on('end', () => {
@@ -19,62 +21,56 @@ var c = https.get('https://api.pinboard.in/v1/posts/all?format=json&auth_token=t
 
 
 var deleteFunction = (url, callback) => {
-   console.log("Broken link detected: " + url);
-   var deleteUrl ='https://api.pinboard.in/v1/posts/delete?url=' + url + '&auth_token=thilko:CC3DBDA67FC2060DE710';
+   var deleteUrl = 'https://api.pinboard.in/v1/posts/delete?url=' + url + '&auth_token=' + apiKey;
    https.get(deleteUrl, (r) => {
-     console.log("Delete result " + r.statusCode);
+     console.log("[" + index + "/" + json.length + "] (404) " + json[index].href + " (DELETED)");
      deleteCounter++;
      callback();
    });
 }
 
-const httpCall = (json, index) => {
+const httpCall = (caller, json, index) => {
   totalCount++;
-   https.get(json[index].href, (result) => {
+   caller.get(json[index].href, (result) => {
       if(result.statusCode == 404){
-           deleteFunction(entry.href, () => {checkUrl(++index, json)});
+        deleteFunction(entry.href, () => { checkUrl(++index, json) });
+      }else if(result.statusCode == 301 || result.statusCode == 302){
+        console.log("[" + index + "/" + json.length + "] (" + result.statusCode + ") " + json[index].href);
+        json[index].href = result.headers.location;
+        checkUrl(index, json);
       }else{
-       console.log(result.statusCode + " " + json[index].href);
+       console.log("[" + index + "/" + json.length + "] (" + result.statusCode + ") " + json[index].href);
        result.on('error', (e) => {
            deleteFunction(json[index].href, () => {checkUrl(++index, json);});
          });
        result.on('data', (e) => {});
        result.on('end', (e) =>  { checkUrl(++index, json);});
       }
-   }).on("error", () =>{ console.log("error"); checkUrl(++index, json);});
+   }).on("error", () =>{
+       console.log("[" + index + "/" + json.length + "] (ERR) " + json[index].href);
+       checkUrl(++index, json);
+   });
 }
 
-function checkUrl(index, json) {
-  console.log(index + " " + json.length);
-  if(index >= json.length){
+function checkUrl(current, allLinks) {
+  if(current >= allLinks.length){
     console.log("Checked " + totalCount + " urls");
     console.log("Deleted " + deleteCounter + " urls");
     return;
   }
-  var entry = json[index];
-     try {
-        if(entry.href.startsWith("https")){
-          httpCall(json, index);
-        }else{
-            totalCount++;
-             http.get(entry.href, (result) => {
-                if(result.statusCode == 404){
-                     deleteFunction(entry.href, () => {checkUrl(++index, json)});
-                }else{
-                 console.log(result.statusCode + " " + entry.href);
-                 result.on('error', (e) => {
-                     deleteFunction(entry.href, () => {checkUrl(++index, json);});
-                   });
-                 result.on('data', (e) => {});
-                 result.on('end', (e) =>  { checkUrl(++index, json);});
-                }
-             }).on("error", () => { console.log("error"); checkUrl(++index, json);});
-        }
-     }catch(e){
-       console.log(e);
-       console.log("unable to reach " + entry.href );
-       checkUrl(++index, json);
-     }
+
+  var entry = allLinks[current];
+ try {
+    if(entry.href.startsWith("https")){
+      httpCall(https, allLinks, current);
+    }else{
+      httpCall(http, allLinks, current);
+    }
+ }catch(e){
+   console.log(e);
+   console.log("unable to reach " + entry.href );
+   checkUrl(++current, allLinks);
+ }
 }
 
 
